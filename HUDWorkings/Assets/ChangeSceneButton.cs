d1919fe7737593bf48079086bf857d036ba2b2ea
Sedawrath <gamerzbis@hotmail.com>
2015-08-22 22:24:31 +1000

    HUD design/elements

---
+++ HUDWorkings/Assets/ChangeSceneButton.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangeSceneButton : MonoBehaviour {
+
+	public void NextLevelButton(int a_index)
+	{
+		Application.LoadLevel (a_index);
+	}
+
+	public void NextLevelButton(string a_lvlName)
+	{
+		Application.LoadLevel (a_lvlName);
+	}
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
